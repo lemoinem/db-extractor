bb6e9ffdbaa8998cdec83f972ebb0419865c3a06
Mathieu Lemoine <mathieu@wemakecustom.com>
2013-05-09 13:56:48 -0400

    First working version

--- index.php
+++ index.php
@@ -2,80 +2,138 @@
 
 require 'openid.php';
 
-try {
-    # Change 'localhost' to your domain name.
-    $openid = new LightOpenID('hosting.wemakecustom.com');
+session_start();
+
+if(!isset($_SESSION['authorized_as'])) {
+  try {
+    $openid = new LightOpenID(gethostname());
     if(!$openid->mode) {
       $openid->identity = 'https://www.google.com/accounts/o8/id';
+      $openid->required = array('contact/email');
       header('Location: ' . $openid->authUrl());
     } elseif($openid->mode == 'cancel') {
       die('User has canceled authentication!');
+    } elseif (!$openid->validate()) {
+      die('Invalid authentication!');
     }
-} catch(ErrorException $e) {
+  } catch(ErrorException $e) {
     echo $e->getMessage();
+  }
+  
+  $id = array();
+  $email = $openid->getAttributes();
+  $email = $email['contact/email'];
+  
+  if(!preg_match('/^([^@]+)@wemakecustom\.com$/', $email, $id))
+    die('Sorry, only accounts from wemakecustom.com are allowed to login (you logged in as "'.$email.'")');
+  
+  $id = $id[1];
+  
+  $_SESSION['authorized_as'] = array($id, $email);
 }
 
-$id = array();
-
-if(!preg_match('^([^@]+)@wemakecustom\.com$', $openid->identity, $id))
-  die('Sorry, only account from wemakecustom.com are allowed to login');
-
-$id = $id[1];
-
-if(!isset($_POST['db'])) : ?>
+if(!isset($_GET['submit'])) : ?>
 <html>
   <head>
      <title>WMC DB Extractor</title>
-     <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js">
-     <script type="text/javascript">
-       function onWPChange() {
-        $("#wp_subform").toggle($("#wordpress").checked);
-       }
-     </script>
+     <style type="text/css">
+       #wp_subform { display: none; }
+       #wordpress:checked ~ #wp_subform { display: block; }
+     </style>
   </head>
   <body>
-    <form>
-      <label for="db">Database: </label><input type="text" name="db" id="db"><br>
-      <input type="checkbox" name="wordpress" id="wordpress" onChange="onWPChange()"><label for="wordpress">Database wordpress to update</label><br>
-      <div id="wp_subform" style="display: none">
-         <label for="src_domain">Source domain: </label><input type="text" name="src_domain" id="src_domain"><br>
-         <label for="dst_domain">Destination domain: </label><input type="text" name="dst_domain" id="dst_domain"><br>
+       <?php
+     $db_access = parse_ini_file('/etc/apache2/.my.cnf');
+$mysql = new mysqli('localhost', $db_access['user'], $db_access['password']);
+$dbs = $mysql->query('SHOW DATABASES');
+$dbs = $dbs->fetch_all();
+        ?>
+    <form method="get">
+      <label for="db">Database: </label><select name="db" id="db">
+  <?php foreach ($dbs as $db) {
+    $dbname = $db[0];
+    if(in_array($dbname, array('mysql', 'information_schema')))
+      continue;
+    echo '<option'.((isset($_GET['db']) && $_GET['db'] == $dbname) ? ' selected' : '').'>'.htmlspecialchars($dbname).'</option>'."\n";
+  } ?>
+      </select><br>
+      <input type="checkbox" name="wordpress" id="wordpress"<?php if(isset($_GET['wordpress'])) echo ' checked'; ?>><label for="wordpress">Database wordpress to update</label><br>
+      <div id="wp_subform">
+  <label for="src_domain">Source domain: </label><input type="text" name="src_domain" id="src_domain" <?php if(isset($_GET['src_domain'])) echo ' value="'.htmlspecialchars($_GET['src_domain']).'"'; ?>><br>
+         <label for="dst_domain">Destination domain: </label><input type="text" name="dst_domain" id="dst_domain" <?php if(isset($_GET['dst_domain'])) echo ' value="'.htmlspecialchars($_GET['dst_domain']).'"'; ?>><br>
       </div> 
-      <input type="checkbox" name="gzip" id="gzip"><label for="gzip">GZIP</label><br>
-      <input type="submit">
+         <input type="checkbox" name="gzip" id="gzip"<?php if(isset($_GET['gzip'])) echo ' checked'; ?>><label for="gzip">GZIP</label><br>
+      <input type="submit" name="submit"><input type="submit" name="get_url" value="Generate URL">
     </form>
   </body>
 </html>
 <?php else: ?>
+<?php
   $forbidden_chars = '\'";$';
-  if(false !== strpbrk($_POST['db'], $forbidden_chars)
-     || false !== strpbrk($_POST['src_domain'], $forbidden_chars)
-     || false !== strpbrk($_POST['dst_domain'], $forbidden_chars))
+  if(false !== strpbrk($_GET['db'], $forbidden_chars)
+     || false !== strpbrk($_GET['src_domain'], $forbidden_chars)
+     || false !== strpbrk($_GET['dst_domain'], $forbidden_chars))
     die('Invalid DB name');
-  <?php
+
+function print_debug($message) {
+  if(/**/false/*/true/**/)
+    trigger_error($message);
+}
+
+function get_dump($var) {
+  ob_start();
+  var_dump($var);
+  $dump = ob_get_contents();
+  ob_end_clean();
+  return $dump;
+}
+
 header('Content-Type: application/force-download');
-header('Content-Disposition: attachment; filename="'.$_POST['db'].'.'.date('Y-m-d_H-i').'.sql'.($_POST['gzip'] ? '.gz' : '').'"');
-$handle = popen('mysqldump --defaults-extra-file=/etc/apache2/.my.cnf --skip-lock-tables --routines --events --triggers \''. $_POST['db']  .'\'', 'r');
-$cleanup = array( function() use ($handle) { pclose($handle); });
-if($_POST['wordpress']) {
+header('Content-Disposition: attachment; filename="'.$_GET['db'].'.'.date('Y-m-d_H-i').'.sql'.(isset($_GET['gzip']) ? '.gz' : '').'"');
+$cmd = 'mysqldump --defaults-extra-file=/etc/apache2/.my.cnf --skip-lock-tables --routines --events --triggers \''. $_GET['db']  .'\'';
+print_debug('Command: '.$cmd);
+$handle = popen($cmd, 'r');
+$err_pipes = array();
+$cleanup = array( function() use ($handle) { print_debug('Cleaning mysqldump'); pclose($handle); });
+if(isset($_GET['wordpress'])) {
   $pipes = array();
-  $process = proc_open(__DIR__.'/wordpress-change-url.php \''.$_POST['src_domain'].'\', \''.$_POST['dst_domain'].'\'',
-                       array(array('pipe', 'r'), $handle),
+  $cmd = __DIR__.'/wordpress-change-url.php \''.$_GET['src_domain'].'\', \''.$_GET['dst_domain'].'\'';
+  trigger_error('Command: '.$cmd);
+  $process = proc_open($cmd,
+                       array($handle, array('pipe', 'w'), array('pipe', 'w')),
                        $pipes);
-  $handle = $pipes[0];
-  $cleanup[] = function () use ($handle) { fclose($handle); };
-  $cleanup[] = function () use ($process) { pclose($process); };
+  print_debug(get_dump($pipes));
+  $handle = $pipes[1];
+  $err = $err_pipes[] = $pipes[2];
+  $cleanup[] = function () use ($err) { print_debug('Cleaning wordpress change stderr'); fclose($err); };
+  $cleanup[] = function () use ($handle) { print_debug('Cleaning wordpress change stdout'); fclose($handle); };
+  $cleanup[] = function () use ($process) { print_debug('Cleaning wordpress change'); proc_close($process); };
 }
-if($_POST['gzip']) {
+if(isset($_GET['gzip'])) {
   $pipes = array();
-  $process = proc_open('gzip',
-                       array(array('pipe', 'r'), $handle),
+  $cmd = 'gzip';
+  trigger_error('Command: '.$cmd);
+  $process = proc_open($cmd,
+                       array($handle, array('pipe', 'w'), array('pipe', 'w')),
                        $pipes);
-  $handle = $pipes[0];
-  $cleanup[] = function () use ($handle) { fclose($handle); };
-  $cleanup[] = function () use ($process) { pclose($process); };
+  print_debug(get_dump($pipes));
+  $handle = $pipes[1];
+  $err = $err_pipes[] = $pipes[2];
+  $cleanup[] = function () use ($err) { print_debug('Cleaning gzip stderr'); fclose($err); };
+  $cleanup[] = function () use ($handle) { print_debug('Cleaning gzip stdout'); fclose($handle); };
+  $cleanup[] = function () use ($process) { print_debug('Cleaning gzip'); proc_close($process); };
 }
-fpassthru($handle);
-foreach($cleanup as $taks) $task();
+$result = fpassthru($handle);
+foreach($err_pipes as $pipe) {
+  if (!is_resource($pipe)) {
+      print_debug(get_dump($pipe));
+      continue;
+    }
+    stream_set_blocking($pipe, 1);
+    while(!feof($pipe))
+      trigger_error(fgets($pipe));
+}
+foreach($cleanup as $task) $task();
+print_debug('fpassthru: '.($result === false ? 'ERROR' : $result));
 ?>
-<?php endif; ?>
+<?php endif;
